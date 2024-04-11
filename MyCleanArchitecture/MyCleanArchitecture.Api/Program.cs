30b114cdfeca08f546b716819456ceb918dac9a0
galeev ruslan <ruslan.nafisovich@gmail.com>
2024-04-11 14:44:25 +0500

    added comment in program class

--- MyCleanArchitecture/MyCleanArchitecture.Api/Program.cs
+++ MyCleanArchitecture/MyCleanArchitecture.Api/Program.cs
@@ -1,8 +1,15 @@
 
 namespace MyCleanArchirecture
 {
+    /// <summary>
+    /// Главный класс приложения.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Точка входа приложения. Здесь происходит конфигурация и запуск веб-сервера.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки.</param>
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
